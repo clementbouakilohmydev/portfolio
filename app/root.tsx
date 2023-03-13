958f8e688ed839c13287479bfa2b5601bade860d
Clément Bouakil <clement.bouakil.ohmydev@gmail.com>
2023-03-13 12:08:36 +0100

    Change website name

--- app/root.tsx
+++ app/root.tsx
@@ -10,7 +10,7 @@ import {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Clément Bouakil - OhMyDev",
   viewport: "width=device-width,initial-scale=1",
 });
 
