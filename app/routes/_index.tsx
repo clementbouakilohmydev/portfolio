fd49f400a0384c100cd4f6975dbe555daecf8a8d
Clément Bouakil <clement.bouakil.ohmydev@gmail.com>
2023-03-13 12:18:41 +0100

    add tailwind

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,7 @@
 export default function Index() {
   return (
     <div>
-      <h1>Welcome to Remix</h1>
-     
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
